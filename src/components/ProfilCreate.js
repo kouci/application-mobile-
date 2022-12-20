6d53b5e3210d8e9ecd31a0fd13e7bef637d3597d
Sidney Audibert <sidney.audibert@gmail.com>
2022-12-20 14:35:29 +0100

    branch creation

---
+++ src/components/ProfilCreate.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TouchableHighlight, FlatList } from 'react-native';
+
+import supabase from "../../src/config/SupabaseClient.js";
+import ActivityItem from "./ActivityItem.js";
+
+const ProfilCreation = () => {
+    
+    
+}
