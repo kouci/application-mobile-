f94962f389444497d0bbf730d94544f6bdb66659
kouci <mansourakociela@gmail.com>
2022-12-20 11:19:37 +0100

    Flatlist added

---
+++ src/components/ActivityItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ActivityItem = ({item}) => {
+  return (
+   <View  style={styles.activityItem}>
+          <Text style={styles.title}>{item.name}</Text>
+          <Text>{item.description}</Text>
+    </View>
+  )
+}
+
+export default ActivityItem
+
+const styles = StyleSheet.create({
+
+   activityItem : {
+     backgroundColor: "#D9E3E9",
+     marginBottom: 10,
+     borderRadius: 5,
+     padding: 10,
+     marginHorizontal: 7,
+     shadowColor: "black",
+     shadowOffset: { width: 5, height: 5 },
+     elevation: 30,
+   },
+   title: {
+     fontWeight: "bold",
+     fontSize: 18,
+     color:"#32749C"
+   },
+   desc: {
+     fontSize:16,
+   }
+ });
--- src/components/List.js
+++ src/components/List.js
@@ -1,28 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import supabase from "../../src/config/SupabaseClient.js";
+import ActivityItem from "./ActivityItem.js";
 
 const List = () => {
   const [activities, setActivities] = useState(null);
-    //console.log(supabase);
+  //console.log(supabase);
+
   const getActivities = async () => {
     try {
-        const {data, error} = await supabase.from('Activity').select()
-        console.log(data)
-        if(data){
-            setActivities(data);
-        }
-      } catch (error) {
-        console.error(error)
+      const { data, error } = await supabase.from("Activity").select();
+      console.log(data);
+      if (data) {
+        setActivities(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
-  }
-  useEffect(() => {
-    getActivities()
-    console.log(activities);
-  },[]);
 
-  return <View></View>;
+  const renderAct = ({item}) => (
+    <ActivityItem item={item}/>
+  )
+
+  
+ 
+
+  useEffect(() => {
+    getActivities();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <FlatList data={activities} renderItem={renderAct}></FlatList>
+    </View>
+  );
 };
 
 export default List;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  activityItem : {
+    backgroundColor: "#D9E3E9",
+    marginBottom: 10,
+    borderRadius: 5,
+    padding: 10,
+    marginHorizontal: 7,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color:"#32749C"
+  },
+  desc: {
+    fontSize:16,
+  }
+});
