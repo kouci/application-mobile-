2153130a1967671c312d317f48dbfc04c33b624a
MOUDDANE Anisse <anisse.mouddane@alumni.univ-avignon.fr>
2023-01-20 20:42:40 +0100

    version final de notre application

--- src/config/SupabaseClient.js
+++ src/config/SupabaseClient.js
@@ -1,11 +1,13 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from "@supabase/supabase-js";
+import {REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_ANON_KEY} from '@env';
 
 // ! Le Code de ce fichier n'est pas à changer hormis sur la branche master 
 
 const supabase = createClient(
-    "https://tbdokxnspzyipuaevygb.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiZG9reG5zcHp5aXB1YWV2eWdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0NTU5OTksImV4cCI6MTk4NzAzMTk5OX0.MLZUtr30gK-5klCzXcjfzx6lTb1_Cia0lQ5GsX5AJLc"
+    REACT_APP_SUPABASE_URL,
+    REACT_APP_SUPABASE_ANON_KEY
 );
+  
 
 export default supabase;
